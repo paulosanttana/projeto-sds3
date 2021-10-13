f95e881ac9d735832a557f0cf8f6d1c27ae3a28e
paulosantana <paulosantana@W108949>
2021-10-13 17:28:41 -0300

    Pagination

--- backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
+++ backend/src/main/java/com/devsuperior/dsvendas/repositories/SellerRepository.java
@@ -1,11 +1,10 @@
 package com.devsuperior.dsvendas.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Service;
 
 import com.devsuperior.dsvendas.entities.Seller;
 
-@Service
+
 public interface SellerRepository extends JpaRepository<Seller, Long>{
 	
 }
